f29c73b709cb6052bbde6a5254c6b3f4c86e6447
maqroll <loteroc@gmail.com>
2021-03-01 19:27:33 +0100

    .

--- src/main/resources/todoListener.js
+++ src/main/resources/todoListener.js
@@ -1,6 +1,6 @@
 // Generated from todo.g4 by ANTLR 4.9.1
 // jshint ignore: start
-import antlr4 from 'antlr4';
+//import antlr4 from 'antlr4';
 
 // This class defines a complete listener for a parse tree produced by todoParser.
 export default class todoListener extends antlr4.tree.ParseTreeListener {
